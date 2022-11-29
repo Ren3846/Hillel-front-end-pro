d42a1d90e1c905922743aaf89d47fb1b6694034b
Renat Chernov <renat3846@gmail.com>
2022-11-29 14:38:26 +0200

    commit first

---
+++ lab_2/average.js
@@ -0,0 +1,6 @@
+let firstNum = +prompt('Введіть перше число');
+let secondNum = +prompt('Введіть друге число');
+let thirdNum = +prompt('Введіть третє число');
+
+let res = (firstNum + secondNum + thirdNum) / 3;
+alert(`Середнє значення ${res}`);
