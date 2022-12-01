c859ccbba997707f0aaab23d6a025ea55b65609a
Renat Chernov <renat3846@gmail.com>
2022-12-01 21:06:20 +0200

    commit lab 3

--- lab2/hours-in-seconds.js
+++ lab2/hours-in-seconds.js
@@ -1,3 +1,5 @@
-const hoursQuantity = +prompt('Введіть кількість годин');
-const secInHours = hoursQuantity * 3600;
+const hoursQuantity = prompt('Введіть кількість годин');
+const hours = parseInt(hoursQuantity);
+
+const secInHours = hours * 3600;
 alert(`У ${hoursQuantity} годинах ${secInHours} секунд`);
