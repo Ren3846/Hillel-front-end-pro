7494e3c1a268df15f920679329439b64bc8b548a
Renat Chernov <renat3846@gmail.com>
2022-12-02 16:37:19 +0200

    commit

--- lab3/calculator.js
+++ lab3/calculator.js
@@ -1,37 +1,24 @@
-// Створити скрипт який повинен виконувати наступне:
+const operation = prompt('Що ви хоче зробити? (add, sub, mult, div)');
+const firstNum = Number(prompt('Введіть перше число'));
+const secondNum = Number(prompt('Введіть друге число'));
 
-// питаємо у користувача, що він хоче зробити (add, sub, mult, div);
-// питаємо у користувача перше число;
-// запитуємо у користувача друге число;
-// виводимо результат дії (add, sub, mult, div) з усіма операндами (Наприклад "2 + 3 = 5").
-
-// const age = 17;
-// const canDriveCar = age >= 18 ? true : false;
-
-// console.log(canDriveCar ? 'Ви можете вести автомобіль' : 'Ви не можете вести автомобіль');
-
-// const anotherAge = 19;
-// const canDrive = anotherAge < 18
-// ? 'Велосипед'
-// : age > 20
-//   ? 'Автобус'
-//   : 'Автомобіль';
-
-// console.log(`Ви можете вести ${canDrive}`);
-
-// const operation = prompt('Що ви хоче зробити? (add, sub, mult, div)');
-// const firstNum = Number(prompt('Введіть перше число'));
-// const secondNum = Number(prompt('Введіть друге число'));
-
-// const res =
-//   operation === 'add'
-//     ? firstNum + secondNum
-//     : operation === 'sub'
-//     ? firstNum - secondNum
-//     : operation === 'mult'
-//     ? firstNum * secondNum
-//     : operation === 'div'
-//     ? firstNum / secondNum
-//     : 'Щось пішло не так(';
-
-// alert(`Операція ${operation}: ${firstNum} ${secondNum} = ${res}`);
+switch (operation) {
+  case 'add':
+    const resAdd = firstNum + secondNum;
+    alert(`${firstNum} + ${secondNum} = ${resAdd}`);
+    break;
+  case 'sub':
+    const resSub = firstNum - secondNum;
+    alert(`${firstNum} - ${secondNum} = ${resSub}`);
+    break;
+  case 'mult':
+    const resMult = firstNum * secondNum;
+    alert(`${firstNum} * ${secondNum} = ${resMult}`);
+    break;
+  case 'div':
+    const resDiv = firstNum / secondNum;
+    alert(`${firstNum} / ${secondNum} = ${resDiv}`);
+    break;
+  default:
+    alert('Щось пішло не так..(');
+}
