6e5245477c19d7ca34cf225480af96d8d3da775b
Renat Chernov <renat3846@gmail.com>
2022-12-02 16:04:38 +0200

    complete navigator

---
+++ lab3/calculator.js
@@ -0,0 +1,37 @@
+// Створити скрипт який повинен виконувати наступне:
+
+// питаємо у користувача, що він хоче зробити (add, sub, mult, div);
+// питаємо у користувача перше число;
+// запитуємо у користувача друге число;
+// виводимо результат дії (add, sub, mult, div) з усіма операндами (Наприклад "2 + 3 = 5").
+
+// const age = 17;
+// const canDriveCar = age >= 18 ? true : false;
+
+// console.log(canDriveCar ? 'Ви можете вести автомобіль' : 'Ви не можете вести автомобіль');
+
+// const anotherAge = 19;
+// const canDrive = anotherAge < 18
+// ? 'Велосипед'
+// : age > 20
+//   ? 'Автобус'
+//   : 'Автомобіль';
+
+// console.log(`Ви можете вести ${canDrive}`);
+
+// const operation = prompt('Що ви хоче зробити? (add, sub, mult, div)');
+// const firstNum = Number(prompt('Введіть перше число'));
+// const secondNum = Number(prompt('Введіть друге число'));
+
+// const res =
+//   operation === 'add'
+//     ? firstNum + secondNum
+//     : operation === 'sub'
+//     ? firstNum - secondNum
+//     : operation === 'mult'
+//     ? firstNum * secondNum
+//     : operation === 'div'
+//     ? firstNum / secondNum
+//     : 'Щось пішло не так(';
+
+// alert(`Операція ${operation}: ${firstNum} ${secondNum} = ${res}`);
