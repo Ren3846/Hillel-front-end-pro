cc10c281e1e0c73f66e209930002c90f87d767ed
Renat Chernov <renat3846@gmail.com>
2022-12-08 19:08:07 +0200

    lab4 - complete

--- lab4/main.js
+++ lab4/main.js
@@ -9,6 +9,8 @@
 // Кожне завдання зі списку повинен виконувати окремий цикл, значення виводити в консоль.
 // Кожне завдання виводити в консоль застосовувючи console.group('Текст завдання')
 
+console.group('Вивести на сторінку в один рядок через кому числа від 10 до 20.');
+
 let res = '';
 for (num = 10; num < 21; num++) {
   if (num === 20) {
@@ -19,6 +21,9 @@ for (num = 10; num < 21; num++) {
 }
 console.log(`${res}`);
 
+console.groupEnd();
+
+console.group('Вивести квадрати чисел від 10 до 20.');
 let res1 = '';
 for (num1 = 10; num1 < 21; num1++) {
   if (num1 === 20) {
@@ -28,23 +33,31 @@ for (num1 = 10; num1 < 21; num1++) {
   }
 }
 console.log(`${res1}`);
+console.groupEnd();
 
+console.group('Вивести');
 for (let num2 = 1; num2 < 11; num2++) {
   console.log(`${num2} * 4 = ${num2 * 4}`);
 }
+console.groupEnd();
 
+console.group('Вивести');
 let res3 = 1;
 for (let num3 = 15; num3 < 36; num3++) {
   res3 = res3 * num3;
 }
 console.log(`${res3}`);
+console.groupEnd();
 
+console.group('Вивести');
 let res4 = 0;
 for (let i = 1; i <= 500; i++) {
   res4 += i;
 }
 console.log(`${res4 / 500}`);
+console.groupEnd();
 
+console.group('Вивести');
 let sum = 0;
 for (let j = 30; j < 80; j++) {
   if (!(j % 2 === 0)) {
@@ -52,3 +65,4 @@ for (let j = 30; j < 80; j++) {
   }
 }
 console.log(`${sum}`);
+console.groupEnd();
