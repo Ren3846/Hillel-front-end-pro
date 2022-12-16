03757dc3f6fdfeaad3cb0e385996747924a7bde4
Renat Chernov <renat3846@gmail.com>
2022-12-16 21:16:17 +0200

    edit function-remove-element / indexOf

--- lab6/functions-remove-element.js
+++ lab6/functions-remove-element.js
@@ -1,14 +1,24 @@
 // Реалізуйте функцію removeElement(array, item), щоб видалити елемент item з масиву array.
 
+// const array = [9, 8, 7, 6, 5, 4, 3, 2, 1];
+// function removeElement(array, item) {
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] == item) {
+//       array.splice(i, 1);
+//       break;
+//     }
+//   }
+// }
+// removeElement(array, 8);
+
+// console.log(array);
+
 const array = [9, 8, 7, 6, 5, 4, 3, 2, 1];
 function removeElement(array, item) {
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] == item) {
-      array.splice(i, 1);
-      break;
-    }
+  const index = array.indexOf(item);
+  if (index !== -1) {
+    array.splice(index, 1);
   }
 }
 removeElement(array, 8);
-
 console.log(array);
