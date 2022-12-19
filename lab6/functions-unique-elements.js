19bd3d871e8adab3daa1d7e248c08f2b5911b753
Renat Chernov <renat3846@gmail.com>
2022-12-19 19:31:28 +0200

    test

--- lab6/functions-unique-elements.js
+++ lab6/functions-unique-elements.js
@@ -3,9 +3,16 @@
 
 const firstArr = [1, 2, 3, 4, 5];
 const secondArr = [5, 6, 7, 8, 9];
+
 function uniqueElements(arr1, arr2) {
-  const uniqueArr = [1];
+  const uniqueArr = firstArr.concat(secondArr);
+  for (let i = 0; i < firstArr.length; i++) {
+    if ((firstArr[i] = secondArr[i])) {
+      uniqueArr.push();
+    }
+  }
   return uniqueArr;
 }
+
 const uniqueArr = uniqueElements(firstArr, secondArr);
 console.log(uniqueArr);
