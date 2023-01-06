3357850f261e7aa1189fc46ffdf5ecaf98381a2b
Renat Chernov <renat3846@gmail.com>
2023-01-06 17:34:33 +0200

    complete

--- lab9/generate-key.js
+++ lab9/generate-key.js
@@ -6,19 +6,18 @@ const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
 function generateKey(count, characters) {
   const array = Array.from(characters);
-  const clone = array.slice(),
-    output = [];
-  count = parseInt(count);
-  if (isNaN(count)) {
-    count = 1;
-  } else {
-    if (count > array.length) count = array.length;
+  const clone = array.slice();
+  const output = [];
+
+  if (count > array.length) {
+    count = array.length;
   }
-  for (var i = 0; i < count; i++) {
+
+  for (let i = 0; i < count; i++) {
     output.push(clone.splice(Math.floor(Math.random() * clone.length), 1));
   }
   return output.join('');
 }
 
-const key = generateKey(16, characters);
+const key = generateKey(10, characters);
 console.log(key); // eg599gb60q926j8i
