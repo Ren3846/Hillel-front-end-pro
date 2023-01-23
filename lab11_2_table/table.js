575747b6815af79455d4d7a82e5f21df4f290187
Renat Chernov <renat3846@gmail.com>
2023-01-23 14:20:37 +0200

    complete

--- lab11_2_table/table.js
+++ lab11_2_table/table.js
@@ -1,18 +1,18 @@
-let totalRows = 10;
-let cellsInRow = 10;
+const totalRows = 10;
+const cellsInRow = 10;
 
 function drawTable() {
-  let div1 = document.getElementById('div1');
+  const div1 = document.getElementById('div1');
 
-  let tbl = document.createElement('table');
+  const tbl = document.createElement('table');
 
   for (let r = 0; r < totalRows; r++) {
-    let row = document.createElement('tr');
+    const row = document.createElement('tr');
 
     for (let c = 1; c <= cellsInRow; c++) {
-      let cell = document.createElement('td');
+      const cell = document.createElement('td');
 
-      let cellText = document.createTextNode(r * 10 + c);
+      const cellText = document.createTextNode(r * 10 + c);
       cell.appendChild(cellText);
       row.appendChild(cell);
     }
