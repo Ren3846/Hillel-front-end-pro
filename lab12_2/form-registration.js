d87df20fff316765745734828e2e6275299f8bcf
Renat Chernov <renat3846@gmail.com>
2023-01-26 15:49:31 +0200

    form

--- lab12_2/form-registration.js
+++ lab12_2/form-registration.js
@@ -10,10 +10,18 @@
 // Кнопка “Зберегти”
 // За натисканням на кнопку замість форми повинна виводитися “таблиця” з даними, які ввів користувач.
 
-const myForm = document.getElementsByTagName('form')[0];
-myForm.addEventListener('submit', func);
-function func() {
-  event.preventDefault();
-  console.log(myForm.name.value);
-  console.log(myForm.name.value);
-}
+// const myForm = document.getElementsByTagName('form')[0];
+// myForm.addEventListener('submit', func);
+// function func() {
+//   event.preventDefault();
+//   console.log(myForm.name.value);
+//   console.log(myForm.name.value);
+// }
+
+button.onclick = function () {
+  // event.preventDefault();
+  let row = table.insertRow();
+  row.insertCell().innerHTML = userName.value;
+  row.insertCell().innerHTML = userLastName.value;
+  row.insertCell().innerHTML = userBirth.value;
+};
