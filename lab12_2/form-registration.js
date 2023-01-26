60237386bc5f3c3484491ca04d757e6e3cc89438
Renat Chernov <renat3846@gmail.com>
2023-01-27 00:36:46 +0200

    form

--- lab12_2/form-registration.js
+++ lab12_2/form-registration.js
@@ -10,18 +10,25 @@
 // Кнопка “Зберегти”
 // За натисканням на кнопку замість форми повинна виводитися “таблиця” з даними, які ввів користувач.
 
-// const myForm = document.getElementsByTagName('form')[0];
-// myForm.addEventListener('submit', func);
-// function func() {
-//   event.preventDefault();
-//   console.log(myForm.name.value);
-//   console.log(myForm.name.value);
-// }
+const formElement = document.getElementById('form1');
+formElement.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const formData = new FormData(formElement);
+  const name = formData.get('first_name');
+  const surname = formData.get('last_name');
+  const birthday = formData.get('birthday');
+  const sex = formData.get('sex');
+  const city = formData.get('city');
+  const adress = formData.get('adress');
+  const selectedCheckBoxes = document.querySelectorAll('input.checkbox:checked');
+  const checkedValues = Array.from(selectedCheckBoxes).map((cb) => cb.value);
 
-button.onclick = function () {
-  event.preventDefault();
   let row = table.insertRow();
-  row.insertCell().innerHTML = userName.value;
-  row.insertCell().innerHTML = userLastName.value;
-  row.insertCell().innerHTML = userBirth.value;
-};
+  row.insertCell().innerHTML = name;
+  row.insertCell().innerHTML = surname;
+  row.insertCell().innerHTML = birthday;
+  row.insertCell().innerHTML = sex;
+  row.insertCell().innerHTML = city;
+  row.insertCell().innerHTML = adress;
+  row.insertCell().innerHTML = checkedValues;
+});
