474e7003368278192b314abc28163e195c697dfb
Renat Chernov <renat3846@gmail.com>
2023-02-09 09:58:17 +0200

    28

--- lab16/main28.js
+++ lab16/main28.js
@@ -4,25 +4,16 @@ let ladder = {
   step: 0,
   up: function () {
     this.step++;
+    return this;
   },
   down: function () {
     this.step--;
+    return this;
   },
   showStep: function () {
-    // показывает текущую ступеньку
     alert(this.step);
+    return this;
   },
 };
 
-// Тепер, якщо нам потрібно зробити кілька послідовних викликів, ми можемо виконати це так:
-
-ladder.up();
-ladder.up();
-ladder.down();
-ladder.showStep(); // 1
-
-// Змініть код методів up, down і showStep таким Таким чином, щоб їх виклик можна було зробити по ланцюжку, наприклад:
-
-ladder.up().up().down().showStep(); // 1
-
-// Такий підхід широко використовується в бібліотеках JavaScript.
+ladder.up().up().down().showStep().down().showStep();
