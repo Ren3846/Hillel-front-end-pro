c6c84004b8f78bc605af1dfbdafa70be1dc3a628
Renat Chernov <renat3846@gmail.com>
2023-02-16 23:29:43 +0200

    api

--- lab17/main.js
+++ lab17/main.js
@@ -5,26 +5,67 @@
 
 const getInput = () => {
   const inputValue = document.getElementsByTagName('input')[0];
-  return `https://pokeapi.co/api/v2/pokemon/${inputValue.value}`;
+  return `https://jsonplaceholder.typicode.com/posts/${inputValue.value}`;
 };
 
-const fetchPosts = () => {
-  fetch(getInput(), { method: 'GET' }).then(async (res) => {
-    if (res.status === 200) {
-      const response = await res.json();
-      console.log(response);
-      const imageSrc = response.sprites.front_default;
-      const imgEl = document.getElementsByTagName('img')[0];
-      imgEl.src = imageSrc;
-      console.log(imageSrc);
-    } else {
-      console.log('error');
-    }
+const getPosts = () => {
+  fetch('https://jsonplaceholder.typicode.com/posts')
+    .then((res) => res.json())
+    .then((res) => res.slice(0, 10))
+    .then(createList)
+    .catch();
+};
+
+const getComments = (id) => {
+  fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`)
+    .then((res) => res.json())
+    .then((res) => console.log(res));
+};
+
+const ulElement = document.querySelector('ul');
+
+const createList = (arr) => {
+  arr.forEach((el) => {
+    const element = document.createElement('li');
+    element.addEventListener('click', () => {
+      getComments(el.id);
+    });
+    element.innerText = el.title;
+    ulElement.append(element);
   });
 };
+getPosts();
+
+// const fetchPosts = () => {
+//   fetch(getInput()).then(async (res) => {
+//     if (res.status === 200) {
+//       const response = await res.json();
+//       div.insertAdjacentHTML('afterend', '<button id="btn">Переглянути коментар</button>');
+//       div.insertAdjacentHTML('afterend', `<p>${response.body}</p>`);
+//       div.insertAdjacentHTML('afterend', `<h1>${response.id}. ${response.title}</h1>`);
+//     } else {
+//       alert('error');
+//     }
+//   });
+// };
 
 const form = document.getElementsByTagName('form')[0];
 form.addEventListener('submit', (event) => {
   event.preventDefault();
-  fetchPosts();
+  getPosts();
 });
+
+// const fetchComments = (id) => {
+//   fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`).then(async (res) => {
+//     if (res.status === 200) {
+//       const response = await res.json();
+//       console.log(response);
+//     } else {
+//       alert('error');
+//     }
+//   });
+// };
+
+// btn.addEventListener('click', (event) => {
+//   fetchComments();
+// });
