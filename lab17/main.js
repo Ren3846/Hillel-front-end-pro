9d7042edfd910f68c3ea9dd663c91152dbfb8202
Renat Chernov <renat3846@gmail.com>
2023-02-17 17:08:59 +0200

    complete fetch jsonplaceholder

--- lab17/main.js
+++ lab17/main.js
@@ -3,38 +3,71 @@
 // то вивести на сторінку блок з постом і зробити кнопку для отримання комкоментарів до посту.
 // Зробити завдання використовуючи проміси, перехопити помилки.
 
+const form = document.getElementsByTagName('form')[0];
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  getPosts();
+});
+
 const getInput = () => {
   const inputValue = document.getElementsByTagName('input')[0];
   return `https://jsonplaceholder.typicode.com/posts/${inputValue.value}`;
 };
 
 const getPosts = () => {
-  fetch('https://jsonplaceholder.typicode.com/posts')
-    .then((res) => res.json())
-    .then((res) => res.slice(0, 10))
-    .then(createList)
-    .catch();
+  fetch(getInput()).then(async (res) => {
+    if (res.ok) {
+      const response = await res.json();
+      console.log(response);
+      createPost(response.id, response.title, response.body);
+      btn.addEventListener('click', (e) => {
+        getComments(response.id);
+        btn.style.display = 'none';
+      });
+    } else {
+      alert('Ошибка:' + res.status);
+    }
+  });
 };
 
 const getComments = (id) => {
-  fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`)
-    .then((res) => res.json())
-    .then((res) => console.log(res));
-};
-
-const ulElement = document.querySelector('ul');
-
-const createList = (arr) => {
-  arr.forEach((el) => {
-    const element = document.createElement('li');
-    element.addEventListener('click', () => {
-      getComments(el.id);
-    });
-    element.innerText = el.title;
-    ulElement.append(element);
+  fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`).then(async (res) => {
+    if (res.ok) {
+      const response = await res.json();
+      console.log(response);
+      createListComments(response);
+    } else {
+      alert('Ошибка:' + res.status);
+    }
   });
 };
-getPosts();
+
+const createPost = (id, title, body) => {
+  post.innerHTML = `
+  <h1>${id}. ${title}</h1>
+  <p id="comment">${body}</p> 
+  <button id="btn">Переглянути коментар</button> `;
+};
+
+const createListComments = (array) => {
+  array.forEach((element) => {
+    comment.insertAdjacentHTML('afterend', `<p>${element.body}</p>`);
+    comment.insertAdjacentHTML('afterend', `<h3>${element.email}</h3>`);
+  });
+};
+
+// const ulElement = document.querySelector('ul');
+
+// const createList = (arr) => {
+//   arr.forEach((el) => {
+//     const element = document.createElement('li');
+//     element.addEventListener('click', () => {
+//       getComments(el.id);
+//     });
+//     element.innerText = el.title;
+//     ulElement.append(element);
+//   });
+// };
 
 // const fetchPosts = () => {
 //   fetch(getInput()).then(async (res) => {
@@ -48,12 +81,6 @@ getPosts();
 //     }
 //   });
 // };
-
-const form = document.getElementsByTagName('form')[0];
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  getPosts();
-});
 
 // const fetchComments = (id) => {
 //   fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`).then(async (res) => {
