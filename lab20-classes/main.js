af08a94de9b82018ebca1c7aa2421252f15e5ca4
Renat Chernov <renat3846@gmail.com>
2023-02-22 16:23:26 +0200

    classes

--- lab20-classes/main.js
+++ lab20-classes/main.js
@@ -23,6 +23,38 @@
 //     конструктор, який приймає один параметр: максимальну кількість квартир;
 //     додати квартиру - метод повинен приймати екземпляр класу Квартира, перевіряти, чи не буде кількість перевищувати максимальну кількість квартир, і якщо це так, додати квартиру, в іншому випадку виводить у консоль відповідне повідомлення.
 
+class Human {
+  constructor(name, gender) {
+    this.name = name;
+    this.gender = gender;
+  }
+}
+
+class Apartment {
+  constructor() {
+    this.residents = [];
+  }
+
+  addResident(resident) {
+    this.residents.push(resident);
+  }
+}
+
+class Building {
+  constructor(maxApartments) {
+    this.apartments = [];
+    this.maxApartments = maxApartments;
+  }
+
+  addApartment(apartment) {
+    if (this.apartments.length < this.maxApartments) {
+      this.apartments.push(apartment);
+    } else {
+      console.log('Maximum number of apartments reached');
+    }
+  }
+}
+
 // В якості демонстраціїї створити:
 
 //     декілька екземплярів класу Людина;
@@ -30,3 +62,20 @@
 //     додадити екземпляри класу Людина до екземплярів класу Квартира;
 //     екземпляр класу Будинок;
 //     додадити екземпляри класу Квартира до екземплярів класу Будинок.
+
+const human1 = new Human('Renat', 'men');
+const human2 = new Human('Stacy', 'women');
+
+const apartment1 = new Apartment();
+const apartment2 = new Apartment();
+
+apartment1.addResident(human1);
+apartment1.addResident(human2);
+apartment2.addResident(human2);
+
+const building1 = new Building(3);
+
+building1.addApartment(apartment1);
+building1.addApartment(apartment2);
+
+console.log(building1);
