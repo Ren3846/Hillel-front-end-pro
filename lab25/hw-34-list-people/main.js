63663bbb28d9399ed8e07793f98ec704cdf4549f
Renat Chernov <renat3846@gmail.com>
2023-02-28 16:46:04 +0200

    complete list-people lab 25

--- lab25/hw-34-list-people/main.js
+++ lab25/hw-34-list-people/main.js
@@ -1,6 +1,7 @@
 // Створити класс  Person class з властивостями name, age, і occupation.
 // Створити в  Person  метод introduction котрий повертає текст "Hi, my name is John. I'm 25 years old and I work as a software engineer."
 // Створіть класс Form котрий в конструкторі очікує html форму.
+
 // В Form створіть метод handleSubmitкотрий дістає значення з полів форми, та створією нового представника класу Person , і виводить представлення себе( introduction) в  DOM.
 // Коли по html формі відбувся submit подія викликайте метод handleSubmit
 // Форма повинна мати поля name, age, і occupation, та кнопку підтвердження.
@@ -15,19 +16,42 @@ class Person {
   }
 
   introduction() {
-    return "Hi, my name is John. I'm 25 years old and I work as a software engineer.";
+    return `Hi, my name is ${this.name}. I'm ${this.age} years old and I work as a ${this.occupation}.`;
   }
 }
 
 class Form {
-  constructor(form) {
-    this.form = form;
-  }
-  handleSubmit() {
-    this.form.value1 = value1;
-    this.form.value2 = value2;
-    this.form.value3 = value3;
+  constructor(formElement) {
+    this.formElement = formElement;
 
-    const person1 = new Person(value1, value2, value3);
+    this.nameInput = formElement.querySelector('#name');
+    this.ageInput = formElement.querySelector('#age');
+    this.occupationInput = formElement.querySelector('#occupation');
+
+    this.formElement.addEventListener('submit', this.handleSubmit.bind(this));
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const person = new Person(
+      this.nameInput.value,
+      this.ageInput.value,
+      this.occupationInput.value,
+    );
+    this.displayPerson(person);
+    this.reset();
+  }
+
+  displayPerson(person) {
+    const introductionElement = document.createElement('p');
+    introductionElement.textContent = person.introduction();
+    document.body.appendChild(introductionElement);
+  }
+
+  reset() {
+    this.formElement.reset();
   }
 }
+
+const formElement = document.querySelector('#person-form');
+const form = new Form(formElement);
