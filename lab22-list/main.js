a2647852727d98ecac19a0cdbac17c4931c11e58
Renat Chernov <renat3846@gmail.com>
2023-03-14 11:35:31 +0200

    rec

--- lab22-list/main.js
+++ lab22-list/main.js
@@ -3,27 +3,19 @@ function generateList(array) {
 
   for (let i = 0; i < array.length; i++) {
     const item = array[i];
-
     const li = document.createElement('li');
-
     if (Array.isArray(item)) {
       li.appendChild(generateList(item));
     } else {
       const text = document.createTextNode(item + i);
-
       li.appendChild(text);
     }
-
     ul.appendChild(li);
   }
-
   return ul;
 }
 
 const array = [1, 2, [3, 4, [5, 6, 7, 8]], 1, 2];
-
 const listContainer = document.getElementById('list-container');
-
 const list = generateList(array);
-
 listContainer.appendChild(list);
